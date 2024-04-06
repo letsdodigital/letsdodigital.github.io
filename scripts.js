9109a2cabddfc011f99d6fdc941fa0bd898476f9
cotswoldsmaker <mark.allan.bailey@gmail.com>
2024-04-06 16:56:39 +0100

    Added .gitignore, clinical-informatician.qmd, ask-a-clinician/index.qmd, LICENSE, who-are-we.qmd, quarterly/index.qmd, posts.qmd, styles.css, _metadata.yml, conferences/2024/index.qmd, conferences/2023/_navbar.qmd, scripts.js, conferences/2022/index.qmd, learn/index.qmd, and .github/workflows/publish.yml

---
+++ scripts.js
@@ -0,0 +1,21 @@
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:1,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:true,
+    smartSpeed:1000,
+});
+
+$(document).ready(function() {
+    $('.owl-item').removeClass('zoom-effect');
+    $('.owl-item.active').addClass('zoom-effect');
+});
+
+owl.on('translated.owl.carousel', function(event) {
+    $('.owl-item').removeClass('zoom-effect');
+    $('.owl-item.active').addClass('zoom-effect');
+});
+
